8ad33993498766c1739a9ba27ac922631b7b3193
Bran van der Meer <branmovic@gmail.com>
2017-03-06 11:43:54 +0100

    Refactored routes into plain js object

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router';
 
-import initialstate from './store/initialstate.json';
 import store from './store/store';
+import initialstate from './store/initialstate.json';
 import Dashboard from './pages/Dashboard/Dashboard';
 import StoryNew from './pages/StoryNew/StoryNew';
 import StoryEdit from './pages/StoryEdit/StoryEdit';
 import NotFound from './pages/NotFound/NotFound';
 
-// @todo
-//  This must call render on top-level component,
-//   skipping re-rendering <Router> itself
+const routes = [{
+    path: '/',
+    component: Dashboard
+}, {
+    path: '/story',
+    component: StoryNew
+}, {
+    path: '/story/:storyId',
+    component: StoryEdit
+}, {
+    path: '*',
+    component: NotFound
+}];
+
 const render = () => {
     ReactDOM.render(
-        <Router history={browserHistory}>
-            <Route path="/" component={Dashboard} />
-            <Route path="/story" component={StoryNew} />
-            <Route path="/story/:storyId" component={StoryEdit} />
-            <Route path="*" component={NotFound} />
-        </Router>,
+        <Router history={browserHistory} routes={routes} />,
         document.getElementById('app')
     );
 };
